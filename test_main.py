b6f17e4cab5451de54d64b5e84c818d1e471604a
Slamova Meruyert <11d.meruyerts@gmail.com>
2024-06-20 03:02:18 +0500

    test change

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"result": "Hello World!"}
 
 def test_read_item():
     response = client.get("/items/42?q=somequery")
